51cde9d7010b00eafa477e250cd33b15c6f5cfa3
szikers <kerszi@protonmail.com>
2021-05-28 23:58:16 +0200

    - added a dictionary with Polish towns - added the ability to create word columns from 1-x columns - cleanup in filenames (the files with diacritics are listed)

--- make_syll.sh
+++
@@ -1,6 +0,0 @@
-#example code to
-#cat 1.syll.ext.pl.txt | grep ^..$
-#cat 1.syll.ext.pl.txt | grep ^...$
-#cat 1.syll.ext.pl.txt | grep ^.*[aeęioóuy]$
-#cat 1.syll.ext.pl.txt | grep ^[^aeęioóuy]*[aeęioóuy]$ > 1.syll.ext.lastvowel.pl.txt
-#egrep '^[a-z]+[0-9]+$' hasla.txt #poczatek znaki, koniec cyfry
